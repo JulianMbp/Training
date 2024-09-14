49c22d53c07e576d22fa47dfc62525aebc7b4d57
julian <julilito09@gmail.com>
2024-09-13 21:53:33 -0500

    adicion de panel de calendario

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,54 @@
 export default function Home() {
   return (
     <main>
-      <header className="fixed w-screen h-20 bg-[#27303F]">
+      <header className=" w-screen h-20 bg-[#27303F]">
         <div className="grid grid-cols-8 ">
           <h1 className=" order-1 text-2xl font-bold text-white text-center col-span-2 pt-3">
             Training
           </h1>
-          <a href="#home" className="order-2 col-span-2 text-white hover:bg-[#ffff] w-full h-full text-center pt-3 m-2 rounded hover:text-gray-900 order-1">
+          <a href="#home" className="order-2 col-span-2 text-white hover:bg-[#ffff] w-full h-full text-center pt-3 m-2 rounded hover:text-gray-900 " >
             Home
           </a>
-          <a href="#home" className="order-3 col-span-2 text-white hover:bg-[#ffff] w-full h-full text-center pt-3 m-2 rounded hover:text-gray-900 order-1">
+          <a href="#dashboard" className="order-3 col-span-2 text-white hover:bg-[#ffff] w-full h-full text-center pt-3 m-2 rounded hover:text-gray-900 ">
             Dashboard
           </a>
           <nav className="order-4 col-span-2 h-10 items-center pl-10 pt-2 text-white">
-           {/*  <img src="https://cdn-icons-png.flaticon.com/512/4245/4245087.png" className="h-10 w-10"></img> */}
             <svg className="h-8 w-8 text-zinc-100"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="17" cy="6" r="2" />  <path d="M9 11a4.97 4.97 0 1 0 3.95 7.95" />  <path d="M19 20l-4 -5h-4l3 -5l-4 -3l-4 1" /></svg>
             Training
           </nav>
         </div>
       </header>
-        <div className="">
+      <nav className="grid grid-cols-[20%_f1] bg-blue-400 h-full md:grid-cols-2 divide-x">
+        <menu id="#dashboard" className=" grid grid-cols-2 h-full w-full order-2 md:grid-cols-4">
+          <div className="border-2 border-slate-500 order-1">
+            1
+          </div>
+          <div className="border-2 border-slate-500 order-2">
+            2
+          </div>
+          <div className="border-2 border-slate-500 order-3">
+            3
+          </div>
+          <div className="border-2 border-slate-500 order-4">
+            4
+          </div>
+          <div className="border-2 border-slate-500 order-5">
+            5
+          </div>
+          <div className="border-2 border-slate-500 order-6">
+            6
+          </div>
+          <div className="border-2 border-slate-500 order-7">
+            7
+          </div>
+          <div className="border-2 border-slate-500 order-8">
+            8
+          </div>
+        </menu>
+        <div className="order-1">
+          hola
         </div>
+      </nav>
     </main>
   );
 }
