6d1c2afa37b4d383644aaeffab3c45ec483249f1
JulianMbp <julian_mtb993@outlook.es>
2024-09-14 11:14:49 -0500

    adicion calendario

--- app/page.tsx
+++ app/page.tsx
@@ -18,35 +18,127 @@ export default function Home() {
           </nav>
         </div>
       </header>
-      <nav className="grid grid-cols-[20%_f1] bg-blue-400 h-full md:grid-cols-2 divide-x">
-        <menu id="#dashboard" className=" grid grid-cols-2 h-full w-full order-2 md:grid-cols-4">
-          <div className="border-2 border-slate-500 order-1">
+
+      <nav id="dashboard" className="grid grid-cols-1 bg-slate-100 h-full w-full divide-x md:grid-cols-4">
+        <menu className="grid grid-cols-4 col-span-3 h-full w-full order-2">
+
+        <div className="border-2 h-20 col-span-4 border-slate-500 ">
+            <h1 className="text-7xl font-semibold ">Octubre</h1>
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
             1
           </div>
-          <div className="border-2 border-slate-500 order-2">
+          <div className="border-2 h-20 border-slate-500 ">
             2
           </div>
-          <div className="border-2 border-slate-500 order-3">
+          <div className="border-2 h-20 border-slate-500 ">
             3
           </div>
-          <div className="border-2 border-slate-500 order-4">
+          <div className="border-2 h-20 border-slate-500 ">
             4
           </div>
-          <div className="border-2 border-slate-500 order-5">
+          <div className="border-2 h-20 border-slate-500 ">
             5
           </div>
-          <div className="border-2 border-slate-500 order-6">
+          <div className="border-2 h-20 border-slate-500 ">
             6
           </div>
-          <div className="border-2 border-slate-500 order-7">
+          <div className="border-2 h-20 border-slate-500 ">
             7
           </div>
-          <div className="border-2 border-slate-500 order-8">
+          <div className="border-2 h-20 border-slate-500">
             8
           </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            9
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            10
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            11
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            12
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            13
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            14
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            15
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            16
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            17
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            18
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            19
+          </div>
+          <div className="border-2 h-20 border-slate-500">
+            10
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            21
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            22
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            23
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            24
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            25
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            26
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            27
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            28
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            29
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            30
+          </div>
+          <div className="border-2 h-20 border-slate-500 ">
+            31
+          </div>
         </menu>
-        <div className="order-1">
-          hola
+
+        <div className="order-1 flex flex-col gap-1">
+          <div>
+            <a href="#entrenamiento" className="flex">
+              <svg  className="h-8 w-8 text-slate-900"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="5" x2="12" y2="19" />  <line x1="5" y1="12" x2="19" y2="12" /></svg>
+              <h3 className="font-thin">Agregar entrenamiento</h3>
+            </a>
+          </div>
+          <div>
+            <a href="#evento" className="flex">
+              <svg className="h-8 w-8 text-slate-900"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="9" r="6" />  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(-30 12 9)" />  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(30 12 9)" /></svg>
+              <h3 className="font-thin">Agregar Competencia</h3>
+            </a>
+          </div>
+          <div>
+            <a href="#recordar" className="flex">
+              <svg className="h-8 w-8 text-slate-900"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="9 11 12 14 20 6" />  <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" /></svg>
+              <h3 className="font-thin">Agregar Recordatorio</h3>
+            </a>
+          </div>
         </div>
       </nav>
     </main>
